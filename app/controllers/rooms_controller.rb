72c9873f9b043a402cf600863271e5567822d681
hytkgami <gagagatrump@gmail.com>
2014-12-29 03:11:44 +0900

    View タイトルを追加

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,8 +1,7 @@
 # coding: utf-8
 
 class RoomsController < ApplicationController
-  # 部屋検索
   def index
-    @rooms = Room.order("room_id") # Roomオブジェクトを取り出す
+    @rooms = Room.order # Roomオブジェクトを取り出す
   end
 end
