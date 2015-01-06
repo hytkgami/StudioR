3410ba6570c3874caa26648dfe66bd7cc4329d71
hytkgami <gagagatrump@gmail.com>
2015-01-07 05:18:18 +0900

    日付,部屋,時間帯の予約の重複を防ぐバリデーションの実装

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -14,6 +14,7 @@ class Booking < ActiveRecord::Base
   validates :to, presence: true, numericality: { greater_than: :from, message: :invalid_end_time }
 
   validate :check_day
+  validate :check_unique
 
   # クラスメソッド
   class << self
@@ -25,6 +26,28 @@ class Booking < ActiveRecord::Base
   def check_day
     if day < Date.today
       errors.add(:day, :invalid)
+    end
+  end
+
+  # 部屋と時間がかぶらないようにする
+  def check_unique
+    invalid_from = false
+    invalid_to = false
+    bookings = Booking.order
+    bookings.each do |booking|
+      if day == booking.day && room_id == booking.room_id
+        if !(from < booking.from && to <= booking.to)
+          invalid_to = true
+        elsif !(from >= booking.to && to > booking.to)
+          invalid_from = true
+        end
+      end
+    end
+    if invalid_from
+      errors.add(:from, "が他の予約と重複します。")
+    end
+    if invalid_to
+      errors.add(:to, "が他の予約と重複します。")
     end
   end
 end
