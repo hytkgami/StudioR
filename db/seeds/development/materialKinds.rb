cbbb7fe6fdb6543cb089cfe701fce81804f6bded
hytkgami <gagagatrump@gmail.com>
2015-01-08 17:04:14 +0900

    CSVを相対パスで指定

--- db/seeds/development/materialKinds.rb
+++ db/seeds/development/materialKinds.rb
@@ -1,6 +1,6 @@
 # coding: utf-8
 require 'csv'
 
-CSV.foreach('/home/kagami/rails/studio/db/seeds/development/Material_kinds.csv','r') do |row|
+CSV.foreach('db/seeds/development/Material_kinds.csv','r') do |row|
   MaterialKind.create(name: row[0].to_s)
 end
--- db/seeds/development/materials.rb
+++ db/seeds/development/materials.rb
@@ -1,7 +1,7 @@
 # coding: utf-8
 require 'csv'
 
-CSV.foreach('/home/kagami/rails/studio/db/seeds/development/Materials.csv','r') do |row|
+CSV.foreach('db/seeds/development/Materials.csv','r') do |row|
   Material.create(
                   name: row[1],
                   kind_id: row[0].to_i
