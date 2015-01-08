3f6b673133ffd9e40867648c4a435d397b0cfda8
hytkgami <gagagatrump@gmail.com>
2015-01-08 16:53:45 +0900

    機材のCSV読み込みを修正

--- db/seeds/development/materialKinds.rb
+++ db/seeds/development/materialKinds.rb
@@ -2,5 +2,5 @@
 require 'csv'
 
 CSV.foreach('/home/kagami/rails/studio/db/seeds/development/Material_kinds.csv','r') do |row|
-  MaterialKind.create(name: row.to_s)
+  MaterialKind.create(name: row[0].to_s)
 end
--- db/seeds/development/materials.rb
+++ db/seeds/development/materials.rb
@@ -1,9 +1,9 @@
 # coding: utf-8
 require 'csv'
 
-CSV.foreach('/home/kagami/rails/studio/db/seeds/development/Materials.csv','r') do |id, row|
+CSV.foreach('/home/kagami/rails/studio/db/seeds/development/Materials.csv','r') do |row|
   Material.create(
-                  name: row,
-                  kind_id: rand(9) + 1
+                  name: row[1],
+                  kind_id: row[0].to_i
                  )
 end
